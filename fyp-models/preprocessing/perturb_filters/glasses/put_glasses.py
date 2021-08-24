2c2983499f8c7598960aac72ad48043f78951a03
Alika Choo <alikachoo5@gmail.com>
2021-08-24 16:38:20 +0800

    Save glasses images

--- fyp-models/preprocessing/perturb_filters/glasses/put_glasses.py
+++ fyp-models/preprocessing/perturb_filters/glasses/put_glasses.py
@@ -2,7 +2,6 @@ import cv2
 
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-glass=cv2.imread('glasses.png')
 
 def put_hat(hat, fc, x, y, w, h):
     face_width = w
@@ -31,17 +30,23 @@ def apply_glasses(ori_path, image_fn, target_glasses):
     """
     # Call apply_glasses(ori_path, image_fn, target_glasses)
     filename=ori_path + image_fn
-    print(filename)
     img=cv2.imread(filename)
+    
+    glass=cv2.imread('glasses.png')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        cv2.imwrite("test.png", img)
+        face_width = w
+        face_height = h
+        hat_width = face_width + 1
+        hat_height = int(0.50 * face_height) + 1
+        
+        glass = cv2.resize(glass, (hat_width, hat_height))
+        
+        for i in range(hat_height):
+            for j in range(hat_width):
+                for k in range(3):
+                    if glass[i][j][k] < 235:
+                        img[y + i - int(-0.20 * face_height)][x + j][k] = glass[i][j][k]
+        cv2.imwrite(target_glasses+image_fn, img)
